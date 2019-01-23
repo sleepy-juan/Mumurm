02910b63a3d8d15407fa7f79d08894c2983eae36
Juan Lee <juanlee@kaist.ac.kr>
2019-01-23 17:40:39 +0900

    Create SignIn & SignUp format

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { Form, Card, Button } from "semantic-ui-react";
 
 class SignIn extends Component {
+  state = {
+    signup: false
+  }
+
+  _onButtonClicked = () => {
+    this.setState({
+      signup: !this.state.signup
+    })
+  }
+
   render() {
     return (
-        <p>SignIn</p>
+      <div style={{position:"absolute", margin: "auto", top:0, right:0, left:0, bottom:0, width:"300px", height:"400px"}}>
+        <Card style={{padding: 10}}>
+          <Card.Content header={this.state.signup ? "Nice to meet you!" : "Welcome Back!"} />
+          <Card.Content extra>
+            <Form>
+              <Form.Input label='Email' placeholder='enter@your.email' />
+              <Form.Input label='Password' placeholder='Password here' type="password" />
+              {this.state.signup ? <Form.Input label='Password Again' placeholder='Password here' type="password" /> : null}
+              <Button onClick={this._onButtonClicked} fluid basic>{this.state.signup ? "You need to sign up first!" : "Sign In or Sign Up!"}</Button>
+            </Form>
+          </Card.Content>
+        </Card>
+      </div>
     );
   }
 }
